734f42b15d49095b571b6b701ae82d0b05908c45
gustavo-dgs <gdgutierrez.19@est.edu.ucab.ve>
2022-04-20 10:44:47 -0600

    Creando Login View

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using pruebasdotnet.Models;
+
+namespace pruebasdotnet.Controllers;
+    
+public class LoginController : Controller
+{
+
+    public IActionResult Index()
+    {
+
+        return View();
+
+    }
+
+}
+
